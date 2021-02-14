fdba4f3d50d857b716f40bbe2744b2229901e4cf
edima <edimabassey2@gmail.com>
2021-02-14 06:38:17 -0800

    twilio code added

---
+++ send_sms.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use Twilio\Rest\Client;
+
+// Your Account SID and Auth Token from twilio.com/console
+$account_sid = 'ACXXXXXXXXXXXXXXXXXXXXX';
+$auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXX';
+// In production, these should be environment variables. E.g.:
+// $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+
+// A Twilio number you own with SMS capabilities
+$twilio_number = "+12053194134";
+
+$client = new Client($account_sid, $auth_token);
+$client->messages->create(
+    // Where to send a text message (your cell phone?)
+    '+2348085124966', 
+    array(
+        'from' => $twilio_number,
+        'body' => 'Emperor emzy sent this message!'
+    )
+);
+
+?>
